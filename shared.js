898a69f926e7647948e2ce71966297495c43480f
Anand Simmy <anandsimmy7@gmail.com>
2020-07-21 13:00:17 +0530

    added condition check

--- shared.js
+++ shared.js
@@ -1,10 +1,12 @@
 const backdrop= document.querySelector('.backdrop')
 const modal= document.querySelector('.modal')
-const modalCloseButton= modal.querySelector('button')
 const selectPlanButtons= document.querySelectorAll('.plan button')
-
+let modalCloseButton=''
 const toggleButton= document.querySelector('.toggle-button')
 const sideNavBar= document.querySelector('.mobile-nav')
+if(modal){
+    modalCloseButton= modal.querySelector('button')
+}
 
 for(let i=0; i<selectPlanButtons.length; i++){
     selectPlanButtons[i].addEventListener('click', ()=>{
@@ -15,12 +17,18 @@ for(let i=0; i<selectPlanButtons.length; i++){
 
 backdrop.addEventListener('click', modalClose)
 
-modalCloseButton.addEventListener('click', modalClose)
+if(modalCloseButton){
+    modalCloseButton.addEventListener('click', modalClose)
+}
 
 function modalClose() {
     backdrop.classList.remove('open')
-    modal.classList.remove('open')
+    if(modal){
+        modal.classList.remove('open')
+    }
+    if(sideNavBar){
     sideNavBar.classList.remove('open')
+    }
 }
 
 toggleButton.addEventListener('click', ()=>{
